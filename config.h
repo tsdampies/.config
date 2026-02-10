69d099e2e0d06de912f16200849af441cb59b072
Trenton Dampies <trentondampies@protonmail.com>
2026-02-10 06:03:15 +0000

    Enable the status bar by setting showbar to 1

--- config.h
+++ config.h
@@ -72,7 +72,7 @@ WIFI_PWR_ON_BAT=off
 /* appearance */
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 0;        /* 0 means no bar */
+static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
